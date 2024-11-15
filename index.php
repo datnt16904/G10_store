970dbaa32163d5bcd4d81a7024de3b868dc4aa18
datnt16904 <tuandathb102@gmail.com>
2024-11-15 13:24:58 +0700

    add header

--- index.php
+++ index.php
@@ -14,50 +14,7 @@
 
 <body>
     <header id="header">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="img/Logo.png" alt="Logo" width="100px" height="auto">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarPrimaryMenu" aria-controls="navbarPrimaryMenu" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarPrimaryMenu">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-
+    <?php include_once "layouts/header.php"; ?>
     </header>
     <main>
         <?php include_once "layouts/slider.php"; ?>
@@ -73,7 +30,7 @@
                                 <h5 class="card-title">Iphone 16 Pro Max</h5>
                                 <p class="card-text"><a href="#">26.000.000 VND</a></p>
                                 <p>Sản phẩm mới nhất của cửa hàng</p>
-                                <a href="#" class="btn btn-primary">Đến mua ngay</a>
+                                <a href="#" class="btn btn-primary">mua ngay</a>
                             </div>
                         </div>
                     </div>
